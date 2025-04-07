f4071c8ed2789452fc9e782cbadb7a77100f06bc
vijay kumar <kumarvijayy036@gmail.com>
2025-04-07 22:19:50 +0530

    decond commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { SignupFormDemo } from "./cmpnnts/SignupFormDemo";
+
+function App() {
+  return (
+    <div className="">
+      <SignupFormDemo />
+    </div>
+  );
+}
+
+export default App;
